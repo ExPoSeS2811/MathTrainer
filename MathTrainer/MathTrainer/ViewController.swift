a4bcf631ac9f1532eccd64b38ac5d3275676d02f
Gleb Rasskazov <glebunium146@gmail.com>
2023-04-06 09:07:05 +0300

    initial commit

---
+++ MathTrainer/MathTrainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathTrainer
+//
+//  Created by MacBook Pro 13 2019 on 4/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
